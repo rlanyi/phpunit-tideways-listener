ffd2005c7d6ba8ba1586290fa92a99addb765667
Robert Lanyi <lanyi.robert.attila@gmail.com>
2018-07-31 11:47:33 +0200

    Add meta data for perftools/xhgui compatibility

--- src/TestListener.php
+++ src/TestListener.php
@@ -42,6 +42,10 @@ final class TestListener implements TestListenerInterface
             return;
         }
 
+        if (!isset($_SERVER['REQUEST_TIME_FLOAT'])) {
+            $_SERVER['REQUEST_TIME_FLOAT'] = \microtime(true);
+        }
+
         \tideways_xhprof_enable(\TIDEWAYS_XHPROF_FLAGS_MEMORY | \TIDEWAYS_XHPROF_FLAGS_CPU);
     }
 
@@ -51,7 +55,53 @@ final class TestListener implements TestListenerInterface
             return;
         }
 
-        $data = \tideways_xhprof_disable();
+        $data['profile'] = \tideways_xhprof_disable();
+
+        // ignore_user_abort(true) allows your PHP script to continue executing, even if the user has terminated their request.
+        // Further Reading: http://blog.preinheimer.com/index.php?/archives/248-When-does-a-user-abort.html
+        // flush() asks PHP to send any data remaining in the output buffers. This is normally done when the script completes, but
+        // since we're delaying that a bit by dealing with the xhprof stuff, we'll do it now to avoid making the user wait.
+        \ignore_user_abort(true);
+        \flush();
+        if (\function_exists('fastcgi_finish_request')) {
+            \fastcgi_finish_request();
+        }
+
+        $uri = \array_key_exists('REQUEST_URI', $_SERVER)
+            ? $_SERVER['REQUEST_URI']
+            : null;
+        if (empty($uri) && isset($_SERVER['argv'])) {
+            $cmd = \basename($_SERVER['argv'][0]);
+            $uri = $cmd . ' ' . \implode(' ', \array_slice($_SERVER['argv'], 1));
+        }
+
+        $time = \array_key_exists('REQUEST_TIME', $_SERVER)
+            ? $_SERVER['REQUEST_TIME']
+            : \time();
+
+        $time_float = \array_key_exists('REQUEST_TIME_FLOAT', $_SERVER)
+            ? $_SERVER['REQUEST_TIME_FLOAT']
+            : \microtime();
+
+        // In some cases there is comma instead of dot
+        $requestTimeFloat = \explode('.', (string)$time_float);
+        if (!isset($requestTimeFloat[1])) {
+            $requestTimeFloat[1] = 0;
+        }
+
+        $requestTs = array('sec' => $time, 'usec' => 0);
+        $requestTsMicro = array('sec' => $requestTimeFloat[0], 'usec' => $requestTimeFloat[1]);
+
+        $data['meta'] = array(
+            'url' => $uri,
+            'SERVER' => $_SERVER,
+            'get' => $_GET,
+            'env' => $_ENV,
+            'simple_url' => $uri,
+            'request_ts' => $requestTs,
+            'request_ts_micro' => $requestTsMicro,
+            'request_date' => date('Y-m-d', $time),
+        );
 
         \file_put_contents($this->fileName($test), \json_encode($data));
     }
